5c1bb6a60a8610bcb1229d50fef7d09037635294
Jvenson Simplice <jvensonsimplice@gmail.com>
2026-01-13 17:52:51 -0500

    Added comments to understand how a data generator works

--- dataGenerator.py
+++ dataGenerator.py
@@ -4,13 +4,30 @@ from tensorflow.python.ops.gen_batch_ops import batch
 
 class DataGenerator(object):
     def __init__(self, prices, batch_size, num_unroll):
-        self.prices = prices
-        self.prices_length = len(self.prices) - num_unroll
-        self.batch_size = batch_size
-        self.num_unroll = num_unroll
+        self.prices = prices # Stores our array of stock prices
+        self.prices_length = len(self.prices) - num_unroll #
+        self.batch_size = batch_size # of sections/sequences (Think of it as the columns) (Also known as the sequences)
+        self.num_unroll = num_unroll # of elements per sequence/batch_size (Think of it as the rows) (Also known as the time steps)
         self.segments = self.prices_length // self.batch_size
 
         self.cursor = [offset * self.segments for offset in range(self.batch_size)]
+
+        """
+        Ex.
+        data_series = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, ...]
+        
+        batch_size = We create 4 Separate sequences: A,B,C,D. Each sequence is a pointer into the data set.
+        Example: A = 0, B = 5, C = 10, D = 15
+        At one time step, we take one value from each sequence. So it would be [0 5 10 15] (Batch_size = 4). Then the next time step would be [1, 6, 11, 16], however 0 -> 1 is a time step and 0 -> 5 is changing sequences.
+                                                                               [1 6 11 16] easier to understand like this. Rows = time steps, columns = sequences.
+                                                                               [2 7 12 17] Each row is ONE time step, 📌 Each column is ONE sequence
+        
+        Way to think about it:
+        Data Set = Long Book
+        Sequence = n # of people reading different chapters (depends on batch_size)
+        Batch  = current word each person is reading (current element/data point)
+        Batch_size = # of people reading;
+        """
 
 
     def next_batch(self):
