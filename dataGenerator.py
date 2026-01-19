a34a884ad7af284367faee6973e89e8244f7bf0f
Jvenson Simplice <jvensonsimplice@gmail.com>
2026-01-19 17:19:49 -0500

    more comments

--- dataGenerator.py
+++ dataGenerator.py
@@ -9,9 +9,9 @@ class DataGenerator(object):
         self.prices_length = len(self.prices) - num_unroll # prices initialized with precaution of going out of bounds.
         self.batch_size = batch_size # of sections/sequences (Think of it as the columns) (Also known as the sequences)
         self.num_unroll = num_unroll # of elements per sequence/batch_size (Think of it as the rows) (Also known as the time steps)
-        self.segments = self.prices_length // self.batch_size # Splits data so sequences do not overlap
+        self.segments = self.prices_length // self.batch_size # Splits data so sequences do not overlap. Set amount of data points to read
 
-        self.cursor = [offset * self.segments for offset in range(self.batch_size)]
+        self.cursor = [offset * self.segments for offset in range(self.batch_size)] # Keeps track of the current position of each segment.
 
         """
         Ex.
@@ -23,38 +23,50 @@ class DataGenerator(object):
                                                                                [1 6 11 16] easier to understand like this. Rows = time steps, columns = sequences.
                                                                                [2 7 12 17] Each row is ONE time step, 📌 Each column is ONE sequence
                                                                                
-        prices_length = What we initialized our price to so it does not go down.
+        *prices_length = What we initialized our price to so it does not go down.
         For example:
         my_arr = [1 2 3 4 5]
-        num_unrolling = 3 (we need this because this is how many values we are going to be time stepping at once)
+        *num_unrolling = 3 (we need this because this is how many values we are going to be time stepping at once)
         i = 2(the element 3 in the array)
         If we try unrolling, we will get an out of bounds error
         
-        segments = basically sliding window all over again so that the sequences do not overlap with the same data
+        *segments = basically sliding window all over again so that the sequences do not overlap with the same data
         my_arr = [99 values]. A = 0:24, B = 24:49 & so on
         
+       *self.cursor = creates an array of elements that point to their respective sequences. Does this by multiplying each generated element in the array by segments
+       Ex.
+       batch_size = 5
+       segments = 2415
+       cursor is tracking the progress. generates a list of [0 1 2 3 4] then multiply each generated element by segments
+       0 * 2415, 1 * 2415, 2 * 2415, 3 * 2415, 4 * 2415. These would be our cursors.
+       We update the cursors when we call next_batch 
         
         Way to think about it:
         Data Set = Long Book
         Sequence = n # of people reading different chapters (depends on batch_size)
         Batch  = current word each person is reading (current element/data point)
         Batch_size = # of people reading (columns)
-        num_unroll = # of timesteps per sequence or number of rows (rows)
+        num_unroll = # of time steps per sequence or number of rows (rows)
         princes_length = threshold of pages to read
         Segment = each person gets their own chapter
+        cursor = like a bookmark for each chapter
         """
 
-    # Generates batch of data and labels
+    # Returns one time step worth of data
     def next_batch(self):
-        batch_data = np.zeros((self.batch_size), dtype=np.float32)
-        batch_labels = np.zeros((self.batch_size), dtype=np.float32)
+        batch_data = np.zeros((self.batch_size), dtype=np.float32) # Initializes a numpy array of 0's for inputs
+        batch_labels = np.zeros((self.batch_size), dtype=np.float32) # Initializes a numpy array of 0's for targets
 
-        for i in range(self.batch_size):
-            if self.cursor[i] + 1 >= self.prices_length:
-                self.cursor[i] = np.random.randint(0, (i + 1) * self.segments)
+        # Iterates over the sequences
+        for i in range(self.batch_size): # i = 0 is column 0, i = 1 is column 1, and so on.
+            if self.cursor[i] + 1 >= self.prices_length: # Condition for if the sequence is about to go over the data set. We need at least one future value for the label
+                # Resets the cursor to safe random position inside its segment
+                self.cursor[i] = np.random.randint(0, (i + 1) * self.segments) # (i + 1) * self.segments ensures it is inside its segment
 
-            batch_data[i] = self.prices[self.cursor[i]]
-            batch_labels[i] = self.prices[self.cursor[i] + np.random.randint(0,5)]
+            # This is the inputs
+            batch_data[i] = self.prices[self.cursor[i]] # Takes the current segment index and store it inside the array
+            # This is the outputs
+            batch_labels[i] = self.prices[self.cursor[i] + np.random.randint(0,5)] # Instead of using cursor + 1, we use cursor with a random int from 0 to 5 for data augmentation
 
             self.cursor[i] = (self.cursor[i] + 1) % self.prices_length
 
