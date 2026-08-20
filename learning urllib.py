11b7cf68f1566d93542b4e497dba1f677e2a3a0b
vensiin <jvensonsimplice@gmail.com>
2026-08-20 06:21:46 -0400

    removed tracked files from repo

--- learning urllib.py
+++
@@ -1,6 +0,0 @@
-import urllib.request  # https://docs.python.org/3/library/urllib.request.html#module-urllib.request
-
-with urllib.request.urlopen("https://docs.python.org/3/howto/urllib2.html") as response:
-    html = response.read().decode("utf-8")
-print(html[:10000])
-
